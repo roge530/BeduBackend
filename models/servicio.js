7fd7f0d90c890011d6bdff42a3f9b4b63386d326
Omar Ontiveros <omar.ontiveros@alumnos.udg.mx>
2022-10-07 20:00:25 -0500

    added associations between servicios and marca

--- models/servicio.js
+++ models/servicio.js
@@ -2,6 +2,7 @@ import { DataTypes } from 'sequelize'
 import { sequelize } from '../config/db.js'
 import {cita_detalle} from './cita_detalle.js'
 import {cita} from './cita.js'
+import { Marca } from './marca.js'
 
 
 export const Servicio=sequelize.define('servicio',{
@@ -17,3 +18,9 @@ export const Servicio=sequelize.define('servicio',{
 })
 
 cita.belongsToMany(Servicio,{through:cita_detalle, foreignKey: 'citaId'});
+Servicio.hasOne(Marca,{
+    foreignKey: {
+        name: "marcaId",
+        allowNull: true
+    }
+    });
