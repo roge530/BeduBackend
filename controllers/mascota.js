46b9d3437143d79c445a215c6e659eaf9078b744
Omar Ontiveros <omar.ontiveros@alumnos.udg.mx>
2022-10-07 22:54:39 -0500

    catch block added to mascota controller

--- controllers/mascota.js
+++ controllers/mascota.js
@@ -4,7 +4,11 @@ export const createMascota = (req, res) => {
     const body = req.body;
     mascota.create(body).then(mascota => {
         res.status(201).json(mascota);
-    });
+    }).catch(err => {
+        res.status(400).json({
+            error: "Elemento(s) inválidos"
+        })
+    })
 }
 
 export const getMascota = async (req, res) => {
@@ -18,12 +22,19 @@ export const getMascotaByCliente = async (req, res) => {
     const result = await mascota.findAll({where: {clienteId: id} })
 }
 
-export const updateMascota = async (req, res) => {
+
+export const updateMascota = (req, res) => {
     const id = req.params.id;
     const mascotaUpdate = req.body;
-    await mascota.update(mascotaUpdate, {where: {id}});
-    const updated = await mascota.findByPk(id);
-    res.status(200).json(updated);
+    mascota.update(mascotaUpdate, {where: {id}})
+        .then(updated => {
+            res.status(200).json(updated);
+        })
+            .catch(err => {
+                res.status(400).json({
+                    error: "Elemento(s) inválidos"
+                })
+            })
 }
 
 export const deleteMascota = async (req, res) => {
