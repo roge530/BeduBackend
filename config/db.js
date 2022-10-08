0b8996242078958089367dc817a8a7788571652c
Rogelio <garu1999@hotmail.com>
2022-10-07 22:55:21 -0500

    db.js acepta nuevo valor

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 import {Sequelize} from 'sequelize'
 import * as dotenv from 'dotenv-safe'
 dotenv.config()
-let uri = `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}` 
+let uri = process.env.DATABASE.URL || `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}` 
 export const sequelize = new Sequelize(uri)
 
   //module.exports = sequelize;
