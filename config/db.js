f048c3ea2e37ac03ae2ad2796748b92a7ddcb138
Rogelio <garu1999@hotmail.com>
2022-10-09 12:21:06 -0500

    correccion para db.js

--- config/db.js
+++ config/db.js
@@ -1,5 +1,12 @@
 import {Sequelize} from 'sequelize'
 import * as dotenv from 'dotenv-safe'
 dotenv.config()
-let uri = `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}` 
-export const sequelize = new Sequelize(uri)
+let uri =  process.env.DATABASE_URL || `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}` 
+export const sequelize = new Sequelize(uri, {
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+})
